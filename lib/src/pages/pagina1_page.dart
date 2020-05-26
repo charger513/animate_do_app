02fd080bd757c5c4a353dc238ca64fc51c23954a
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-26 17:36:49 -0500

    Diseño de la página 1

--- lib/src/pages/pagina1_page.dart
+++ lib/src/pages/pagina1_page.dart
@@ -1,11 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Pagina1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Animate_do'),
+        actions: <Widget>[
+          IconButton(
+            icon: FaIcon(FontAwesomeIcons.twitter),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.navigate_next),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: FaIcon(FontAwesomeIcons.play),
+        onPressed: () {},
+      ),
       body: Center(
-        child: Text('Hello World'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.new_releases, color: Colors.blue, size: 40),
+            Text(
+              'Título',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+            Text(
+              'Soy un texto pequeño',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            Container(
+              width: 220,
+              height: 2,
+              color: Colors.blue
+            ),
+          ],
+        ),
       ),
     );
   }
